4233b5437083754f0cc7dfd4e188d337dd07f1e3
nicolas kellum <nicolas.kellum@hotmail.fr>
2019-02-08 16:27:58 +0100

    adding sick script with 42 api

---
+++ scripting/apiscript.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+TOKEN="a019d87c2368e0bd7ac14aeaa7e832e9f444f7b4b6e0771afcba7eabe2c0d242"
+
+VALUE=$(curl -sH "Authorization: Bearer $TOKEN" "https://api.intra.42.fr/v2/users/$1" | tr -d '{}')
+
+
+#curl -X POST --data "grant_type=client_credentials&client_id=05c45c4c5fdc8ac228bb68f5dee9f83ca84ca3d4fe3ab168483d1e4c28d8ca2e&client_secret=d564ea66f71f4fca77d1e169740a89bd036e750c7cb414a6428149885a2923c7" https://api.intra.42.fr/oauth/token
+echo
+if [ -z "$VALUE" ] || [ -z "$1" ]
+then
+      echo "Incorrect username or no username specified."
+      echo "usage: ./apiscript.sh intra_username"
+      exit 2
+fi
+
+echo -n "Name: "
+echo $VALUE | grep -Po '"displayname":.*?[^\\]",' | tr -d '",' | cut -f2 -d":"
+echo -n "Email: "
+echo $VALUE | grep -Po '"email":.*?[^\\]",' | tr -d '",' | cut -f2 -d":"
+echo -n "Phone number: "
+echo $VALUE | grep -Po '"phone":.*?[^\\]",' | tr -d '",' | cut -f2 -d":"
+echo -n "Location: "
+echo $VALUE | grep -Po '"location":.*?[^\\]",' | tr -d '",' | cut -f2 -d":"
+echo -n "Correction Points: "
+echo $VALUE | grep -Po '"correction_point":.*?[^\\]",' | tr -d '",' | grep -oP '(?<=:).*?(?=p)'
+echo -n "Wallet: "
+echo $VALUE | grep -Po '"wallet":.*?[^\\]",' | head -1 | tr -d '",' | cut -f2 -d":" | cut -f1 -d"g" | awk '{print $1"₳"}'
+echo -n "Level: "
+echo $VALUE | grep -Po '"level":.*?[^\\]",' | head -1 | tr -d '",' | cut -f2 -d":" | cut -f1 -d"s"
+
+python -mwebbrowser https://cdn.intra.42.fr/users/$1.jpg
+
+#$VALUE | grep -Po '"image_url":.*?[^\\]",' | tr -d '",' | cut -d: -f2-
+#grep -Po '"text":.*?[^\\]",'
